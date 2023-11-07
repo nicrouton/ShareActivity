7cbf5f750fe40b3e4c5d157f1249e91ee8edfd58
nickc <tug61145@temple.edu>
2023-11-07 12:16:58 -0500

    Added intent object and passed it to createChooser

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,9 +1,11 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            val actionInt : Intent = Intent().apply{
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "this is my text" )
+                type = "text/plain"
+            }
+            val newInt = Intent.createChooser(actionInt,null)
+            startActivity(actionInt)
 
         }
     }
